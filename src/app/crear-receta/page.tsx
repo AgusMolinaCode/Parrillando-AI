5995ecf649d0b533f5eb86bc5287c1b014106b07
Agustin Molina <am.motos@hotmail.com>
2024-01-15 21:55:29 -0300

    Refactor variable names for clarity and consistency

--- src/app/crear-receta/page.tsx
+++ src/app/crear-receta/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <div className='text-center font-bold text-gray-500 text-xl py-7 px-2'>
+        Crea las mejores recetas y compártelas con tus amigos.
+      </div>
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
--- src/components/sections/MiPerfil/DeleteButton.tsx
+++ src/components/sections/MiPerfil/DeleteButton.tsx
@@ -1,45 +1,56 @@
-'use client';
+"use client";
 
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Button, ButtonGroup } from "@nextui-org/react";
 
 interface DeleteButtonProps {
-  id: string;
-  url: string;
+	id: string;
+	url: string;
 }
 
 async function deleteItem(url: string, id: string) {
-  const response = await fetch(`${url}/${id}`, {
-    method: "DELETE",
-  });
-  const data = await response.json();
-  return data;
+	const response = await fetch(`${url}/${id}`, {
+		method: "DELETE",
+	});
+	const data = await response.json();
+	return data;
 }
 
 const DeleteButton: React.FC<DeleteButtonProps> = ({ id, url }) => {
-  const [isDeleting, setIsDeleting] = useState(false);
-  const router = useRouter();
+	const [isDeleting, setIsDeleting] = useState(false);
+	const router = useRouter();
 
-  const handleDelete = async () => {
-    setIsDeleting(true);
-    await deleteItem(url, id);
-    router.refresh();
-    setIsDeleting(false);
-  };
+	const handleDelete = async () => {
+		setIsDeleting(true);
+		try {
+			await deleteItem(url, id);
+			router.refresh();
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setTimeout(() => setIsDeleting(false), 5000); // espera 1 segundo antes de cambiar el estado
+		}
+	};
 
-  return (
-    <>
-      <ButtonGroup className="border rounded-xl">
-        <Button color="primary" variant="light" size="md">
-          Editar
-        </Button>
-        <Button onClick={handleDelete} color="danger" variant="light" size="md">
-          {isDeleting ? "Eliminando..." : "Eliminar"}
-        </Button>
-      </ButtonGroup>
-    </>
-  );
+	return (
+		<>
+			<ButtonGroup className="border rounded-xl">
+				<Button color="primary" variant="light" size="md">
+					Editar
+				</Button>
+				<Button
+					onClick={handleDelete}
+					color="danger"
+					variant="light"
+					size="md"
+					isDisabled={isDeleting ? true : false}
+				>
+					{isDeleting ? "Eliminando..." : "Eliminar"}
+				</Button>
+			</ButtonGroup>
+		</>
+	);
 };
 
 export default DeleteButton;
