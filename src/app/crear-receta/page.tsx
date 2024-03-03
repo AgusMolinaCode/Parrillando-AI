ec2aa2787ad8d56c9abcd6dec00e704fd7f1ea9d
Agustin <am.motos@hotmail.com>
2024-03-03 01:07:44 -0300

    Agregadas dependencias de Next.js y Cloudinary

--- src/app/crear-receta/page.tsx
+++ src/app/crear-receta/page.tsx
@@ -15,126 +15,126 @@ import { useRouter } from "next/navigation";
 const notify = () => toast.success("Receta creada con éxito!");
 
 const page = () => {
-	const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-	const [authorId, setAuthorId] = useState<string | null>(null);
-	const { userId } = useAuth();
-	const router = useRouter();
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const [authorId, setAuthorId] = useState<string | null>(null);
+  const { userId } = useAuth();
+  const router = useRouter();
 
-	useEffect(() => {
-		if (userId) {
-			getAuthorId(userId).then(id => {
-				setAuthorId(id);
-			});
-		}
-	}, [userId]);
+  useEffect(() => {
+    if (userId) {
+      getAuthorId(userId).then((id) => {
+        setAuthorId(id);
+      });
+    }
+  }, [userId]);
 
-	const [title, setTitle] = useState("");
-	const [category, setCategory] = useState<Categoria | null>(null);
-	const [description, setDescription] = useState("");
-	const [photo, setPhoto] = useState("");
-	const [ingredients, setIngredients] = useState([{ name: "", quantity: "" }]);
-	const [steps, setSteps] = useState([{ description: "" }]);
-	const [likesCount, setLikesCount] = useState(0);
-	const [creando, setCreando] = useState(false);
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState<Categoria | null>(null);
+  const [description, setDescription] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [ingredients, setIngredients] = useState([{ name: "", quantity: "" }]);
+  const [steps, setSteps] = useState([{ description: "" }]);
+  const [likesCount, setLikesCount] = useState(0);
+  const [creando, setCreando] = useState(false);
 
-	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-		setCreando(true);
-		event.preventDefault();
-		const recipe = {
-			title,
-			category: category?.title,
-			description,
-			photo: [photo],
-			authorId,
-			likesCount: likesCount,
-			ingredients: ingredients.map(ingredient => ({
-				name: ingredient.name,
-				quantity: ingredient.quantity,
-			})),
-			steps: steps.map(step => ({
-				description: step.description,
-			})),
-		};
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    setCreando(true);
+    event.preventDefault();
+    const recipe = {
+      title,
+      category: category?.title,
+      description,
+      photo: [photo],
+      authorId,
+      likesCount: likesCount,
+      ingredients: ingredients.map((ingredient) => ({
+        name: ingredient.name,
+        quantity: ingredient.quantity,
+      })),
+      steps: steps.map((step) => ({
+        description: step.description,
+      })),
+    };
 
-		const response = await fetch(`${apiUrl}/recetas`, {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(recipe),
-		});
+    const response = await fetch(`${apiUrl}/recetas`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(recipe),
+    });
 
-		if (!response.ok) {
-			const message = `Error al crear la receta: ${response.status} ${
-				response.statusText
-			}. ${await response.text()}`;
-			throw new Error(message);
-		}
+    if (!response.ok) {
+      const message = `Error al crear la receta: ${response.status} ${
+        response.statusText
+      }. ${await response.text()}`;
+      throw new Error(message);
+    }
 
-		const responseData = await response.json();
+    const responseData = await response.json();
 
-		notify();
-		FormReset();
-		setCreando(false);
-		router.push('/perfil');
-	};
+    notify();
+    FormReset();
+    setCreando(false);
+    router.push("/perfil");
+  };
 
-	const FormReset = () => {
-		setTitle("");
-		setCategory(null);
-		setDescription("");
-		setPhoto("");
-		setIngredients([{ name: "", quantity: "" }]);
-		setSteps([{ description: "" }]);
-	};
+  const FormReset = () => {
+    setTitle("");
+    setCategory(null);
+    setDescription("");
+    setPhoto("");
+    setIngredients([{ name: "", quantity: "" }]);
+    setSteps([{ description: "" }]);
+  };
 
-	return (
-		<div>
-			<div>
-				<Toaster position="top-center" />
-			</div>
-			<div className="max-w-lg flex flex-col justify-center mx-auto mt-6 mb-10 sm:border sm:border-gray-300 rounded-xl sm:p-3 px-2">
-				<form onSubmit={handleSubmit}>
-					<div className="pb-7 pt-2 px-2">
-						<h1 className="text-left font-bold text-gray-900 text-2xl">
-							Mi Receta.
-						</h1>
-						<p className="text-left font-semibold text-gray-500 text-lg">
-							Crea las mejores recetas y compártelas con tus amigos.
-						</p>
-					</div>
-					<div className="flex flex-col">
-						<div className="flex flex-col items-center justify-center px-2 content-center">
-							<FormFields
-								title={title}
-								setTitle={setTitle}
-								category={category as Categoria}
-								setCategory={setCategory}
-								description={description}
-								setDescription={setDescription}
-								photo={photo}
-								setPhoto={setPhoto}
-							/>
-						</div>
-					</div>
-					<Ingredients
-						ingredients={ingredients}
-						setIngredients={setIngredients}
-					/>
-					<Steps steps={steps} setSteps={setSteps} />
-					<div className="border-t-1 border-gray-300 my-4">
-						<Button
-							color="success"
-							className="mt-3 flex justify-center mx-auto"
-							type="submit"
-						>
-							{creando ? "Creando..." : "Crear receta"}
-						</Button>
-					</div>
-				</form>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div>
+        <Toaster position="top-center" />
+      </div>
+      <div className="max-w-lg flex flex-col justify-center mx-auto mt-6 mb-10 sm:border sm:border-gray-300 rounded-xl sm:p-3 px-2">
+        <form onSubmit={handleSubmit}>
+          <div className="pb-7 pt-2 px-2">
+            <h1 className="text-left font-bold text-gray-900 text-2xl">
+              Mi Receta.
+            </h1>
+            <p className="text-left font-semibold text-gray-500 text-lg">
+              Crea las mejores recetas y compártelas con tus amigos.
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <div className="flex flex-col px-2 content-center">
+              <FormFields
+                title={title}
+                setTitle={setTitle}
+                category={category as Categoria}
+                setCategory={setCategory}
+                description={description}
+                setDescription={setDescription}
+                photo={photo}
+                setPhoto={setPhoto}
+              />
+            </div>
+          </div>
+          <Ingredients
+            ingredients={ingredients}
+            setIngredients={setIngredients}
+          />
+          <Steps steps={steps} setSteps={setSteps} />
+          <div className="border-t-1 border-gray-300 my-4">
+            <Button
+              color="success"
+              className="mt-3 flex justify-center mx-auto"
+              type="submit"
+            >
+              {creando ? "Creando..." : "Crear receta"}
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- src/components/sections/MiPerfil/Crear Receta/FormFields.tsx
+++ src/components/sections/MiPerfil/Crear Receta/FormFields.tsx
@@ -1,92 +1,116 @@
 import React from "react";
-import { Input, Select, SelectItem, Textarea } from "@nextui-org/react";
+import { Button, Input, Select, SelectItem, Textarea } from "@nextui-org/react";
 import { Categoria, List } from "@/libs/interfaces/categorias";
 import { FaUser, FaTag } from "react-icons/fa";
+import { CldUploadWidget } from "next-cloudinary";
 
 interface Props {
-	title: string;
-	setTitle: (value: string) => void;
-	category: Categoria;
-	setCategory: (value: Categoria) => void; 
-	description: string;
-	setDescription: (value: string) => void;
-	photo: string;
-	setPhoto: (value: string) => void;
+  title: string;
+  setTitle: (value: string) => void;
+  category: Categoria;
+  setCategory: (value: Categoria) => void;
+  description: string;
+  setDescription: (value: string) => void;
+  photo: string;
+  setPhoto: (value: string) => void;
 }
 
 const FormFields = ({
-	title,
-	setTitle,
-	category,
-	setCategory,
-	description,
-	setDescription,
-	photo,
-	setPhoto,
+  title,
+  setTitle,
+  category,
+  setCategory,
+  description,
+  setDescription,
+  photo,
+  setPhoto,
 }: Props) => {
-	return (
-		<>
-			<Input
-				type="text"
-				label="Titulo"
-				placeholder="Añade un título"
-				labelPlacement="outside"
-				onChange={e => setTitle(e.target.value)}
-				value={title}
-				startContent={<FaUser size={20} color="#000" />}
-				radius="lg"
-				className="my-6"
-				isRequired
-			/>
-			<Select
-				value={category?.title}
-				label="Categoría"
-				placeholder="Selecciona tu categoría"
-				labelPlacement="outside"
-				onChange={e => {
-					const selectedCategory = List.find(
-						cat => cat.title === e.target.value
-					);
-					if (selectedCategory) {
-						setCategory(selectedCategory);
-					}
-				}}
-				startContent={<FaTag size={20} color="#000" />}
-				radius="lg"
-				className="my-6"
-				isRequired
-			>
-				{List.map(category => (
-					<SelectItem key={category.title} value={category.title}>
-						{category.title}
-					</SelectItem>
-				))}
-			</Select>
-			<Textarea
-				value={description}
-				onChange={e => setDescription(e.target.value)}
-				label="Description"
-				labelPlacement="outside"
-				placeholder="Descripción de la receta"
-				className="mb-6"
-				isRequired
-			/>
-			{/* <Input
-				type="file"
-				value={photo}
-				onChange={e => setPhoto(e.target.value)}
-				placeholder="URL de la foto"
-				label="Foto"
-				labelPlacement="outside"
-				className="mb-6"
-				multiple
-				classNames={{
-					input: "p-2",
-				}}
-				isRequired
-			/> */}
-		</>
-	);
+  return (
+    <>
+      <Input
+        type="text"
+        label="Titulo"
+        placeholder="Añade un título"
+        labelPlacement="outside"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        startContent={<FaUser size={20} color="#000" />}
+        radius="lg"
+        className="my-6"
+        isRequired
+      />
+      <Select
+        value={category?.title}
+        label="Categoría"
+        placeholder="Selecciona tu categoría"
+        labelPlacement="outside"
+        onChange={(e) => {
+          const selectedCategory = List.find(
+            (cat) => cat.title === e.target.value
+          );
+          if (selectedCategory) {
+            setCategory(selectedCategory);
+          }
+        }}
+        startContent={<FaTag size={20} color="#000" />}
+        radius="lg"
+        className="my-6"
+        isRequired
+      >
+        {List.map((category) => (
+          <SelectItem key={category.title} value={category.title}>
+            {category.title}
+          </SelectItem>
+        ))}
+      </Select>
+      <Textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        label="Description"
+        labelPlacement="outside"
+        placeholder="Descripción de la receta"
+        className="mb-6"
+        isRequired
+      />
+      {/* <Input
+        type="file"
+        value={photo}
+        onChange={(e) => setPhoto(e.target.value)}
+		onClick={() => open()}
+        placeholder="URL de la foto"
+        label="Foto"
+        labelPlacement="outside"
+        className="mb-6"
+        classNames={{
+          input: "p-2",
+        }}
+        isRequired
+      /> */}
+      <CldUploadWidget
+        options={{
+          maxFiles: 1,
+          cropping: true,
+        }}
+        uploadPreset="parrillando"
+        onSuccess={(results) => {
+          if (results.info && typeof results.info !== "string") {
+            console.log(results.info.secure_url);
+            setPhoto(results.info.secure_url);
+          }
+        }}
+      >
+        {({ open }) => {
+          return (
+            <div className="mb-6 grid">
+              <Button value={photo} onClick={() => open()}>
+                Subir Imagen
+              </Button>
+            </div>
+          );
+        }}
+      </CldUploadWidget>
+    </>
+  );
 };
 
 export default FormFields;
